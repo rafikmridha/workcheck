7ad53c3c5b6469f556bc75b5cfeb6216cbf13e6d
rafikmridha <rafikmridha99@gmail.com>
2023-03-24 13:04:19 +0600

    index added

--- introduction-to-javascript/array/array.js
+++ introduction-to-javascript/array/array.js
@@ -1,1 +1,5 @@
 var playersAge = [35, 30, 28, 29,];
+var playersName = ['rafik', 'hasib', 'rohim', 'korim'];
+
+console.log(playersAge, playersName .length);
+console.log( playersName ,playersAge .length );
---
+++ introduction-to-javascript/array/index.js
@@ -0,0 +1,10 @@
+var numbers = [12, 24, 25, 48, 47, 50];
+console.log(numbers);
+var elements = numbers[0];
+console.log(elements);
+numbers[0] = 100;
+console.log(numbers);
+
+
+var positionIndex = numbers.indexOf(50);
+console.log(positionIndex);
