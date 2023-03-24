9e1d07f58ad2934b3a8cc00cf6c7693d3b74c9cd
rafikmridha <rafikmridha99@gmail.com>
2023-03-24 19:18:27 +0600

    array index practicee

--- introduction-to-javascript/array/array.js
+++ introduction-to-javascript/array/array.js
@@ -2,4 +2,31 @@ var playersAge = [35, 30, 28, 29,];
 var playersName = ['rafik', 'hasib', 'rohim', 'korim'];
 
 console.log(playersAge, playersName .length);
-console.log( playersName ,playersAge .length );
+console.log(playersName, playersAge.length);
+
+var playersName = ['rafik', 'safik', 'hasib', 'imdadul', 'hamid'];
+var playersMoney = [55, 60, 80, 100, 150];
+var positionIndex = playersName.indexOf(60);
+console.log(positionIndex);
+var elements = playersName[0];
+console.log(elements)
+var elements = playersName[1];
+console.log(elements)
+var elements = playersName[2];
+console.log(elements)
+var elements = playersName[3];
+console.log(elements)
+var elements = playersName[4];
+console.log(elements)
+var elements = playersMoney[(0)];
+console.log(elements);
+var elements = playersMoney[(1)];
+console.log(elements);
+var elements = playersMoney[(2)];
+console.log(elements);
+var elements = playersMoney[(3)];
+console.log(elements);
+var elements = playersMoney[(4)];
+console.log(elements);
+console.log(playersName, playersMoney.length);
+console.log(playersMoney, playersName.length);
